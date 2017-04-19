c2a63fc4b8a445d9e95be08f2f925cf3b152aa19
Akhilesh Tharakan <akhilesh.tharakan@gmail.com>
2017-04-19 12:13:10 +0530

    Comments Update

--- app_s7_a1.js
+++ app_s7_a1.js
@@ -3,9 +3,11 @@
 
 var area;
 var r = parseInt(prompt("Enter the value for radius: "));
+/*Function is defined here, Where the r value which user entered is being taken here*/
 function areaofcircle(r)
 {
 	return 3.14*(r*r);
 }
 window.alert("The area of the circle with radius " + r + " is : " + areaofcircle(r));
+/* The Final calculated value is printed here*/
 
